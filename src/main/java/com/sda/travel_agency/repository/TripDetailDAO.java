cec99fc871a7cb06ed6dbc21999d77f83833dd5e
alexmatei88 <58986113+alexmatei88@users.noreply.github.com>
2020-02-05 20:10:32 +0200

    Update TripDetailDAO.java

--- src/main/java/com/sda/travel_agency/repository/TripDetailDAO.java
+++ src/main/java/com/sda/travel_agency/repository/TripDetailDAO.java
@@ -26,7 +26,13 @@ public class TripDetailDAO {
         try {
             session = HibernateUtil.getSessionFactory().openSession();
             transaction = session.beginTransaction();
-
+            
+// partea asta poate sa fie o metoda separata , poate fi chiar si in clasa HotelDAO. ce faci aici nu afecteaza cu nimic
+            // tranzactia, sau cel putin asa pare
+            // daca ai fi vrut ca trip detail sa nu fie salvat daca hotelavailability e falise/ null 
+            // iti lipseste un if (hotelAvailability= cum vrei tu ) {transaction rollback }
+            // chiar si asa poti muta intr o metoda separata care sa ti returneze un HotelAvailability.. te bazezi ca getSingleResult iti returneaza
+            // hibernate exception si daca e asta nu ai nici o camera... cam superficial..
             Query roomsQuery = session.createNamedQuery("hotelAvailability.find");
             roomsQuery.setParameter("hotel", trip.getHotel());
             roomsQuery.setParameter("startDate", trip.getCheckInDate());
@@ -37,17 +43,28 @@ public class TripDetailDAO {
             session.persist(tripDetail);
 
             //update user amount
+            
+            // din moment ce incepi o tranzactie sus si o commiti sau rollback in catch/finally
+            // fiecare bucata de cod care updateaza o entitate poate sa fie pusa intr o alta metoda
+            // poate fi private ca doar aici o folosesti care sa faca update ul, eventual fara sa porneasca o alta tranzactie
+            // poti face toate pregatirile obiectului pe care vrei sa l updatezi intr o alta metoda si aici sa faci doar executeUpdate
+            // ramai cu 2 linii de cod
             double userNewAmount = user.getTotalAmount() + tripDetail.getAmount();
             Query userAmountUpdate = session.createNamedQuery("user.updateAmount");
             userAmountUpdate.setParameter("amount", userNewAmount);
             userAmountUpdate.setParameter("id", user.getId());
             userAmountUpdate.executeUpdate();
 
+            //tranzactia ta incepe sus si se termina
             //update rooms availability
+            
+            //daca diferenta de aici da < 0 ? updatezi cu numar negativ ?
+            // ce inseamna sa ajungi sa updatezi cu numar negativ ??
             int newSingleRoomsAvailable = hotelAvailability.getSingleRooms() - tripDetail.getSingleRooms();
             int newDoubleRoomsAvailable = hotelAvailability.getDoubleRooms() - tripDetail.getDoubleRooms();
             int newExtraBedsAvailable = hotelAvailability.getExtraBeds() - tripDetail.getExtraBeds();
 
+            // vezi ce am scris la update user
             Query roomsUpdate = session.createNamedQuery("hotelAvailability.updateRooms");
             roomsUpdate.setParameter("singleRooms", newSingleRoomsAvailable);
             roomsUpdate.setParameter("doubleRooms", newDoubleRoomsAvailable);
@@ -56,6 +73,8 @@ public class TripDetailDAO {
             roomsUpdate.executeUpdate();
 
             //update flights available seats
+            
+            //vezi ce am scris la update user
             int departureFlightSeats = trip.getDepartureFlight().getAvailableSeats() - numberOfPersons;
             Query departureFlightUpdate = session.createNamedQuery("flight.updateAvailableSeats");
             departureFlightUpdate.setParameter("availableSeats", departureFlightSeats);
